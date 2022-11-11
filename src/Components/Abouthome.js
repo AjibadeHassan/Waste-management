b3777d4bdb44346c936c171d8fe7da56e16ee5f7
hassan <hassanajibade17@gmail.com>
2022-11-11 21:43:09 +0100

    abouthome

--- src/Components/Abouthome.js
+++ src/Components/Abouthome.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import Logo from '../Assets/Profile.png'
 
 
 const Abouthome = () => {
   return (
-    <div Abouthome_container>
-        About
+    <div className='Abouthome_container'>
+      <h1>About Us</h1>
+       <div className='Abothome_Div'>
+       <section className='Img_container'>
+          <img src={Logo} alt='profile'/>
+        </section>
+        <section className='About_Info'>
+          <h1>We've  got what you need!</h1>
+          <p>
+Blockages are usually a sign that the sewage disposal system
+ either requires maintenance or repair. If your septic system has
+  a blockage or is full you could be experiencing the following:
+   – Toilets running slow – Shower is running slow – A smell coming
+    up from the septic air vent or drain – Excessive grass growth around the
+     septic tank – Sewage coming out the air vent or the drains. If you are 
+     experiencing any of the symptoms above call us now on. We are concerned with
+your septic tank evacuation. We cover all over lagos. We are prompt.
+</p>
+
+<h3>Our Services has been Unmatched</h3>
+        </section>
+       </div>
     </div>
   )
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import Abouthome from './Abouthome'
 import Homepage from './Homepage'
-import '../Css/Style.css'
-import '../Css/Mobile.css'
-
 const Main = () => {
   return (
     <div >
