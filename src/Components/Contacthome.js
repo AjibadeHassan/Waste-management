42f86beb0c3e4874b2028d39b196ef8a64d3ea81
hassan <hassanajibade17@gmail.com>
2022-11-13 06:50:38 +0100

    last sun

---
+++ src/Components/Contacthome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contacthome = () => {
+  return (
+    <div className='Contacthome'>
+        <h1>Contact Us</h1>
+        <section className='Contacthome_div'>
+
+        </section>
+    </div>
+  )
+}
+
+export default Contacthome
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,12 +4,14 @@ import Homepage from './Homepage'
 import { IoIosCall } from 'react-icons/io'
 import { BsWhatsapp } from 'react-icons/bs'
 import Servicehome from './Servicehome'
+import Contacthome from './Contacthome'
 const Main = () => {
   return (
     <div>
         <Homepage/>
         <Abouthome/>
         <Servicehome/>
+        <Contacthome/>
         <div className='Reach_out'>
           <IoIosCall className='Reach_out__icons'/>
           <BsWhatsapp className='Reach_out__icons'/>
