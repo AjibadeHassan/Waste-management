b474e044c539e7409c8c7512d9422e21ba29e614
hassan <hassanajibade17@gmail.com>
2022-11-13 02:27:24 +0100

    first sun commit

--- src/Components/Servicehome.js
+++ src/Components/Servicehome.js
@@ -1,17 +1,45 @@
 import React from 'react'
-
+import { MdOutlineHighQuality,MdBlock } from 'react-icons/md'
+import { BsTrash,BsLaptop } from 'react-icons/bs'
 const Servicehome = () => {
   return (
     <div className='Servicehome_container'>
-        <section className='Servicehome_Card'>
-          <p></p>
+      <h1>Our Services</h1>
+      <p>Fumi Sewage Services provide the widest range of Sewage 
+        treatment and waste disposal of hazardous and non hazardous waste.</p>
+       <div className='Serv_cards'>
+       <section className='Servicehome_Card'>
+        <BsTrash className='Serv_icon'/>
+        <h3>Sewage Disposal</h3>
+          <p>We handle domestic and industrial
+             waste in liquid, solid or sludge.</p>
         </section>
         <section className='Servicehome_Card'>
-          <p></p>
+         
+          <MdOutlineHighQuality className='Serv_icon'/>
+          <h3>Quality Products</h3>
+          <p>We offer guaranteed goods on our products</p>
         </section>
         <section className='Servicehome_Card'>
-          <p></p>
+          
+          <MdBlock className='Serv_icon'/>
+          <h3>Sewage Blockages</h3>
+          <p>We open up sewage blockages along the tracks and pack
+             the solid waste at a negotiable fee on site.</p>
         </section>
+        <section className='Servicehome_Card'>
+         
+          <BsLaptop className='Serv_icon'/>
+          <h3>Up To Date</h3>
+          <p>All dependencies are kept current
+             to keep things fresh.</p>
+        </section>
+        <section className='Servicehome_Card'>
+          <h3>We Solve Issues Like:</h3>
+          <p>Toilets running slow, Shower is running slow,
+             Excessive grass growth around the septic tank etc.</p>
+        </section>
+       </div>
     </div>
   )
 }
