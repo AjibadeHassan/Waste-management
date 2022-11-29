3d1bcc21387eae3eeb4cfab4306172eaeae3ca79
hassan <hassanajibade17@gmail.com>
2022-11-29 19:40:45 +0100

    storage tues

--- src/Components/AdminDash.js
+++ src/Components/AdminDash.js
@@ -1,35 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { collection, addDoc, getDocs } from 'firebase/firestore'
-import { db } from '../lib/Base'
+import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { db, storage } from '../lib/Base';
 import Footer from './Footer';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 
-const AdminDash = (e) => {
+const AdminDash = () => {
    
-    // const delcollectionRef = collection(db, 'equipments')
-   
-   
-    // function deleteEquipment(id){
-    //     deleteDoc(equipcollectionRef, id)
-    //     .then(()=> console.log('Item deleted'))
-    //     .catch(err=> console.log(err))
-    // }
     const equipcollectionRef = collection(db, 'equipments')
      const [equipments, setequipments] = useState([])
-        getEquipments()
-    
-        
-   
-        
-      
-    
-    useEffect(()=>{
+     const [info, setInfo] = useState(null)
+     const [progress, setProgress] = useState(0)
+     const [myUrl, setMyUrl] = useState()
+
+     const Upload =(event)=>{
+        const File = event.target.files[0]
+        const Any = URL.createObjectURL(File)
+        setInfo(Any)
+        uploadStorage(Any)
         console.log(equipments)
+     }
+
+const uploadStorage = (file)=>{
+    if(!file) {
+        console.log('no data available')
+    };
+    const storageRef = ref(storage, `/files/${file.name}`)
+    const UploadTask = uploadBytesResumable(storageRef, file)
+
+    UploadTask.on(
+        "state_changed",
+        (snapshot) =>{
+            const prog = Math.round((snapshot.bytesTransferred / snapshot.totalBytes)*100)
+            setProgress(prog)
+            console.log(progress)
+        },
+        (err)=> {
+            console.log(err)
+        },
+        ()=>{
+            getDownloadURL(UploadTask.snapshot.ref)
+            .then(url => {
+                setMyUrl(url)
+            })
+
+        }
+    )
+}
+      getEquipments()
+   
+    useEffect(()=>{
+        // console.log(equipments)
     },[equipments])
     
  function getEquipments(){
         getDocs(equipcollectionRef, 'equipments')
         .then(response=>{
-            console.log(response)
+            // console.log(response)
             const equip = response.docs.map(doc=> ({
                 data: doc.data(),
                 id: doc.id,
@@ -39,24 +65,26 @@ const AdminDash = (e) => {
     }
     const handleSubmit = (e)=>{
         e.preventDefault()
-        if (name === '' || price === '') {
+        if (name === '' || price === '' || myUrl === '') {
             return
         }
         // alert(name)
         // alert(price)
         const myData = {
             name: name,
-            price: price
+            price: price,
+            imageUrl: myUrl
         }
         addDoc(equipcollectionRef, myData)
-        .then(response=>{
-            console.log(response.id)
-        }).catch(err=> console.log(err.message))
+        // .then(response=>{
+        //     console.log(response.id)
+        // }).catch(err=> console.log(err.message))
         // window.location.reload()
+    console.log (myUrl)
     } 
     const[name,setName] = useState('')
     const [price, setPrice] =useState('')
-    // console.log(setName(), name)
+
   return (
     <div className='Admin_container'>
         <h1>My products</h1>
@@ -64,7 +92,7 @@ const AdminDash = (e) => {
             {
                 equipments.map((props)=>(
                     <section className='Product_Card'key={props.id}>
-                    <img src='' alt='a pix' />
+                    <img src={props.data.imageUrl} alt='a pix' />
                     <h3> {props.data.name} </h3>
                     <p> #{props.data.price} </p>
                     <button className='Order_btn'>Update Price</button>
@@ -74,7 +102,15 @@ const AdminDash = (e) => {
             }
         </div>
         <h1>Upload Product</h1>
+       
         <form className='Upload_form' onSubmit={handleSubmit}>
+        <div className='upload_cont'>
+            <img src={info} alt='a pix' />
+        </div>
+        <label htmlFor='choose'>
+               <input id='choose' type='file'  onChange={Upload} hidden/>
+               <div  className='Order_btnn'>Upload Image</div>
+           </label>
           
         <input  value={name} placeholder='Product Name' onChange={e=> setName(e.target.value)} type='text' />
         <input  value={price} placeholder='Price' onChange={e=> setPrice(e.target.value)} type='number' />
--- src/lib/Base.js
+++ src/lib/Base.js
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDCZph32QUUb5fEacZs2ToZaygOBl2ofIo",
-  authDomain: "fumi-waste.firebaseapp.com",
-  projectId: "fumi-waste",
-  storageBucket: "fumi-waste.appspot.com",
-  messagingSenderId: "478775416223",
-  appId: "1:478775416223:web:987b4c00267ce8115359aa"
+  apiKey: "AIzaSyD-ij3-jGeVZf1hE3gEbxuMGIQrR5U9g8k",
+  authDomain: "fumiwaste.firebaseapp.com",
+  projectId: "fumiwaste",
+  storageBucket: "fumiwaste.appspot.com",
+  messagingSenderId: "157893196634",
+  appId: "1:157893196634:web:c64bcf9a7002c58e7b9163",
+  measurementId: "G-6TTL6TZXGM"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app); 
